8e49c6a5c704424de388cb6e1c183e5e3c207781
kianomoomi <kianomoomi1380@gmail.com>
2020-05-01 14:27:39 +0430

    manage menus

---
+++ Test/src/main/java/View/CommandProcessor.java
@@ -0,0 +1,16 @@
+package View;
+
+import java.util.Scanner;
+
+public class CommandProcessor {
+    private Scanner scanner;
+    public CommandProcessor() {
+        scanner = new Scanner(System.in);
+    }
+    public void runWithMenu() {
+        Menu.setScanner(this.scanner);
+        Menu currentMenu = new MainMenu();
+        currentMenu.show();
+        currentMenu.execute();
+    }
+}
