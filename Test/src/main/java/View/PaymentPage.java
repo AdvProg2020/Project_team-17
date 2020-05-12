16085982d31ee01ccb2cc296b883fd237084f696
kianomoomi <kianomoomi1380@gmail.com>
2020-05-13 00:18:42 +0430

    PaymentPage done

--- Test/src/main/java/View/PaymentPage.java
+++ Test/src/main/java/View/PaymentPage.java
@@ -1,5 +1,8 @@
 package View;
 
+import Controller.AccountsManager.CustomerAbilitiesManager;
+import Models.Accounts.Customer;
+
 public class PaymentPage extends Menu {
     public PaymentPage(Menu parentMenu) {
         super("Payment", parentMenu);
@@ -7,11 +10,29 @@ public class PaymentPage extends Menu {
 
     @Override
     public void show() {
-
+        System.out.println("you are in payment page");
+        System.out.println("1.finish your purchase");
+        System.out.println("2.back");
     }
 
     @Override
     public void execute() {
+        int input = Integer.parseInt(scanner.nextLine());
+        if (input == 1) {
+            pay();
+        } else if (input == 2) {
+            parentMenu.show();
+            parentMenu.execute();
+        }
+    }
 
+    private void pay() {
+        Customer customer = RegisterAndLoginMenu.getCurrentCustomer();
+        try {
+            CustomerAbilitiesManager.checkAndPay(customer, DiscountCodePage.getDiscountCode());
+            System.out.println("Finish");
+        } catch (Exception e) {
+            e.getMessage();
+        }
     }
 }
