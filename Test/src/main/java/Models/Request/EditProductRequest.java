0e4f900e330baf924afcc433777b8d8d832f5819
kianomoomi <kianomoomi1380@gmail.com>
2020-05-16 18:10:03 +0430

    EditProductRequest done

--- Test/src/main/java/Models/Request/EditProductRequest.java
+++ Test/src/main/java/Models/Request/EditProductRequest.java
@@ -2,20 +2,39 @@ package Models.Request;
 
 import Models.Accounts.Manager;
 import Models.Accounts.Seller;
+import Models.Category;
 import Models.Enums.ProductEnum;
 import Models.Enums.RequestTypeEnum;
 import Models.Product;
 
 public class EditProductRequest extends Request {
     private Product product;
+    private String field;
+    private String newContentForThisField;
 
-    public EditProductRequest(Seller seller, Manager manager, Product product) {
-        super("Edit product ---> " + allRequests.size() + 1, seller, manager);
+    public EditProductRequest(Seller seller, Manager manager, Product product, String field, String newContentForThisField) {
+        super("Edit product ---> " + (allRequests.size() + 1), seller, manager);
         this.type = RequestTypeEnum.EDIT_PRODUCT;
         this.product = product;
+        this.field = field;
+        this.newContentForThisField = newContentForThisField;
     }
 
+    @Override
     public void accept() {
+        if (field.equals("name")) {
+            product.setName(newContentForThisField);
+        } else if (field.equals("companyName")) {
+            product.setCompanyName(newContentForThisField);
+        } else if (field.equals("description")) {
+            product.setExplanation(newContentForThisField);
+        } else if (field.equals("seller")) {
+            product.setSeller(Seller.getSellerByName(newContentForThisField));
+        } else if (field.equals("price")) {
+            product.setPrice(Double.parseDouble(newContentForThisField));
+        } else if (field.equals("category")) {
+            product.setCategory(Category.getCategoryByName(newContentForThisField));
+        }
         product.setProductState(ProductEnum.ACCEPTED);
     }
 
