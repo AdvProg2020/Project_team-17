9b54cbbd246f0af5a6f7ebdecdfd5e1b432f7f6b
nonaghazizadeh <nonaghazizadeh@yahoo.com>
2020-07-18 12:27:07 +0430

    add some method for Supporter

--- AP17/src/main/java/Models/Accounts/Supporter.java
+++ AP17/src/main/java/Models/Accounts/Supporter.java
@@ -1,8 +1,31 @@
 package Models.Accounts;
 
+import java.util.ArrayList;
+
 public class Supporter extends Account {
+
+    private static ArrayList<Supporter> allSupporters = new ArrayList<>();
+
     public Supporter(String userName, String firstName, String lastName, String email, String phoneNumber, String password, double credit) {
         super(userName, firstName, lastName, email, phoneNumber, password, credit);
+    }
+
+    public static boolean isThereSupporterWithUserName(String username) {
+        for (Supporter supporter : allSupporters) {
+            if (supporter.getUserName().equals(username)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static Supporter getSupporterByUserName(String username) {
+        for (Supporter supporter : allSupporters) {
+            if (supporter.getUserName().equals(username)) {
+                return supporter;
+            }
+        }
+        return null;
     }
 
     @Override
