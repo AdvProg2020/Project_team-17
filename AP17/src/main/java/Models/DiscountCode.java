0f3246e67345d2667d138bc7af6c9c67949b9ed7
Youngbizman <youngbizmann@gmail.com>
2020-07-24 23:28:41 +0430

    DiscountCode.java debug

--- AP17/src/main/java/Models/DiscountCode.java
+++ AP17/src/main/java/Models/DiscountCode.java
@@ -2,10 +2,11 @@ package Models;
 
 import Models.Accounts.Customer;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
-public class DiscountCode {
+public class DiscountCode implements Serializable {
     private String discountCode;
     private LocalDate startDate;
     private LocalDate endDate;
@@ -14,7 +15,6 @@ public class DiscountCode {
     private int countDiscountCode;
     private Customer customer;
     private static ArrayList<DiscountCode> allDiscountCodes = new ArrayList<>();
-
 
     public DiscountCode(String discountCode, LocalDate startDate, LocalDate endDate, double discountPercent,
                         double maxDiscountAmount, int countDiscountCode, Customer customer) {
